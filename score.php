5915b95b662013c6a5f4e1a6b48bae42029ef445
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-04-23 16:50:17 -0400

    Update score.php

--- score.php
+++ score.php
@@ -15,28 +15,44 @@ if ($conn->connect_error) {
 
 // Get the user_id based on the provided username
 $user = $_POST['username'];
-$sql = "SELECT id, password FROM GOL_users WHERE username = ?";
+$sql = "SELECT id FROM GOL_users WHERE username = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $user);
 $stmt->execute();
 $result = $stmt->get_result();
 $row = $result->fetch_assoc();
 
-// Verify the password
-if (password_verify($_POST['password'], $row['password'])) {
+if ($row) {
     $user_id = $row['id'];
 
-    // Insert the new score and elapsed time into GOL_scores
+    // Check if a record exists for the user in the GOL_scores table
+    $sql = "SELECT * FROM GOL_scores WHERE user_id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
     $max_population = $_POST['max_population'];
     $elapsed_time = $_POST['elapsed_time'];
-    $sql = "INSERT INTO GOL_scores (user_id, max_population, elapsed_time) VALUES (?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("iii", $user_id, $max_population, $elapsed_time);
-    $stmt->execute();
+
+    if ($row) {
+        // Update the existing record with new max_population and elapsed_time
+        $sql = "UPDATE GOL_scores SET max_population = ?, elapsed_time = ? WHERE user_id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("iii", $max_population, $elapsed_time, $user_id);
+        $stmt->execute();
+    } else {
+        // Insert a new record with max_population and elapsed_time
+        $sql = "INSERT INTO GOL_scores (user_id, max_population, elapsed_time) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("iii", $user_id, $max_population, $elapsed_time);
+        $stmt->execute();
+    }
 
     echo "Score submitted successfully";
 } else {
-    echo "Invalid username or password";
+    echo "Invalid username";
 }
 
 $stmt->close();
